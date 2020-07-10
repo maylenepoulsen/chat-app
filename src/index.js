2428414c58e662158a9bccf3ddcbda99d7ead78a
maylenepoulsen <mayleneski@hotmail.com>
2020-07-10 15:59:32 -0500

    added event acknowledgement for client and server

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const http = require('http')
 const path = require('path')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -19,9 +20,26 @@ io.on('connection', (socket) => {
   //to send an event from the server to the clients we use socket.emit
   //the first argument is the event name (we can name it whatever we want)
   socket.emit('message', 'Welcome!')
+  socket.broadcast.emit('message', 'A new user has joined!')
 
-  socket.on('sendMessage', (message) => {
+  socket.on('sendMessage', (message, callback) => {
+    const filter = new Filter()
+
+    if(filter.isProfane(message)) {
+      return callback('Profanity is not allowed!')
+    }
+
     io.emit('message', message)
+    callback()  
+  })
+
+  socket.on('sendLocation', (coords, callback) => {
+    io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    callback()
+  })
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left!')
   })
 })
 

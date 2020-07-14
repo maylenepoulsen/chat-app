43b00a6105db3a7db57af56c3f29d783566fa468
maylenepoulsen <mayleneski@hotmail.com>
2020-07-14 11:39:48 -0500

    added in Mustache templates for the messages and location

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,43 @@
-const socket = io() 
+const socket = io()
+
+const messageForm = document.querySelector('#message-form')
+const formInput = messageForm.querySelector('input')
+const formButton = messageForm.querySelector('button')
+const messages = document.querySelector('#messages')
+
+//Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationTemplate = document.querySelector('#location-template').innerHTML
+
+const locationButton = document.querySelector('#send-location')
 
 //socket.on receives messages from the server
 socket.on('message', (message) => {
-    console.log(message)
+    const html = Mustache.render(messageTemplate, {
+      message
+    })
+    messages.insertAdjacentHTML('beforeend', html)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+socket.on('locationMessage', (url) => { 
+  const html = Mustache.render(locationTemplate, {
+    url
+  })
+  messages.insertAdjacentHTML('beforeend', html)
+})
+
+messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
+
+    formButton.setAttribute('disabled', 'disabled')
+
     const message = e.target.elements.message.value
     
     socket.emit('sendMessage', message, (error) => {
+      formButton.removeAttribute('disabled')
+      formInput.value = ''
+      formInput.focus()
+
       if (error) {
         return console.log(error)
       }
@@ -18,16 +46,18 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     })
 })
 
-document.querySelector('#send-location').addEventListener('click', () => {
+locationButton.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser.')
     }
 
+    locationButton.setAttribute('disabled', 'disabled')
     navigator.geolocation.getCurrentPosition((position) => {
       socket.emit('sendLocation', {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude 
       }, () => {
+        locationButton.removeAttribute('disabled')
         console.log('Location shared')
       })
     })
